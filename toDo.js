8461ba268fb975a21bd0b56add57f26b0ff03afd
arianecrestani <ariane.mrv@gmail.com>
2021-05-11 17:00:14 +0200

    add some changes

--- toDo.js
+++ toDo.js
@@ -1,17 +1,16 @@
 const taskButton = document.getElementById("click");
 const inputTask = document.getElementById("inputTask")
+const addToDo = document.getElementById ("addToDo")
 
 const generateButtonToDo = () => {
-    showList(inputTask.value)
 
+    showList(inputTask.value)
+   
 };
 taskButton.addEventListener("click", generateButtonToDo);
+addToDo.addEventListener('click', deleteTask);
 
-const removeToDo =(toDo)=> {
-  const removeDiv = document.createElement('div');
-  removeDiv.className = 'removeDivSection';
-  section.appendChild(removeDiv);
-};
+
 
 const showList = (toDo) => {
   const container = document.getElementsByClassName("container")[0];
@@ -20,18 +19,34 @@ const showList = (toDo) => {
   container.appendChild(addToDo(toDo));
 };
 
-const addToDo = (toDo) => {
+const deleteTask = () => {
+ let taks = []
 
+};
+
+const addToDo = (toDo) => {
   const addItem = document.createElement("div");
   addItem.id ="addToDo";
   addItem.innerHTML = toDo;
   return addItem
 
 };
-const listTodo = (toDo) =>{
+
+const toDoList = (list) => {
+
+  const listDiv = document.createElement("div");
+  listDiv .id ="listDiv";
+  listDiv .innerHTML = list;
+  listDiv.appendChild(addItem)
+  return listDiv
 
 
 }
+// function createDivSection() {
+//   const section = document.createElement("div");
+//   section.className = "divSection";
+//   return section;
+// }
 
 
 
