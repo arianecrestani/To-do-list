3d48954bed4aef52abbcbdc589124475260868a7
arianecrestani <ariane.mrv@gmail.com>
2021-05-20 13:33:50 +0200

    add div create delete button

--- toDo.js
+++ toDo.js
@@ -1,11 +1,14 @@
 const taskButton = document.getElementById("click");
 const inputTask = document.getElementById("inputTask");
+const deleteBtn = document.getElementById("deleteBtn");
+const itemList = document.getElementById("itemList");
 
 const taskButtonEventHandler = () => {
   if (inputTask.value.length > 0) {
     showList(inputTask.value);
   }
 };
+
 const removeSpaces = () => {
   inputTask.value = inputTask.value.trim(" ");
 };
@@ -16,13 +19,17 @@ const enterPress = (e) => {
   }
 }
 
+const deleteBtnEventHandler = () => {
+  itemList.innerHTML = '';
+}
+
 taskButton.addEventListener("click", taskButtonEventHandler);
 inputTask.addEventListener("blur", removeSpaces);
 inputTask.addEventListener("keypress", enterPress);
+deleteBtn.addEventListener("click", deleteBtnEventHandler)
 
 const showList = (toDo) => {
-  const container = document.getElementsByClassName("add-item")[0];
-  container.appendChild(createItem(toDo));
+  itemList.appendChild(createItem(toDo));
   inputTask.value = "";
 };
 
